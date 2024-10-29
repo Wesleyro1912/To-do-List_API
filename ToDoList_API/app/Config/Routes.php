cd49c2f052ee3c96196363348a30af5b3e831441
Wesley Rodrigues <wesleyrodriguesmoreira02@gmail.com>
2024-10-29 00:15:22 -0300

    3° commit

--- ToDoList_API/app/Config/Routes.php
+++ ToDoList_API/app/Config/Routes.php
@@ -11,7 +11,7 @@ $routes->get('/', 'Home::index');
 $routes->group('task', static function ($routes) {   
     $routes->get('/', 'Task::index', ['as' => 'task']);
     $routes->post('/store', 'Task::store', ['as' => 'task.store']);
-    $routes->get('/edit/(:num)', 'Task::edit/$1', ['as' => 'task.edit']);
+    $routes->get('/edit/(:num)', 'Task::edit/$1/', ['as' => 'task.edit']);
     $routes->put('/update/(:num)', 'Task::update/$1', ['as' => 'task.update']);
     $routes->delete('/delete/(:num)', 'Task::delete/$1', ['as' => 'task.delete']);
     $routes->patch('/status/(:any)', 'Task::status/$1', ['as' => 'task.status']);
